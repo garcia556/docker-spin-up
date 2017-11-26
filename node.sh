773e7ac5148fa86f1ceb1d8ff705f15c8b580f61
Dmitry Glavchev <dmitry.glavchev@gmail.com>
2017-11-26 09:29:07 -0500

    Added multiple packages processing for i mode

--- node.sh
+++ node.sh
@@ -15,26 +15,27 @@ if [ ! -f ${APP} ]; then
 	APP_DEMO="1"
 fi
 
+# save mode and gather args
 mode=$1
-cmd="-v"
+shift
+args=""
+for var in "$@"
+do
+	args="${args} ${var}"
+done
 
+# set npm command
+cmd="-v"
 if [ -z "${mode}" ]; then
 	echo "Mode not set"
 	exit 1
 else
 	case ${mode} in
 		"i") # install package
-			cmd="install --silent $2"
+			cmd="install --silent ${args}"
 			;;
 		"r") # run
 			# save args and pass to node script
-			shift
-			args=""
-			for var in "$@"
-			do
-				args="${args} ${var}"
-			done
-
 			cmd="start --silent ${args}"
 			;;
 	esac
